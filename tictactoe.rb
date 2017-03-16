1694855cb25f7c13e69728a33537e723e92977c1
mm86 <madhuvanmahesh@gmail.com>
2017-03-16 14:09:46 -0700

    feat: Create states and behaviors within classes Board and Square
    Class Board: Create a state @squares within initialize method and set it to a hash with key, value pairs
    Keys are numbered from 1 to 9 and values are set to a class Square object. Create a method called get_square_at(key) that takes a key value as an argument and returns the value stored for that particular key.
    
    Class Square: Create a state called marker. The value for the state marker is provided by the class Board's instance variables squares.

--- tictactoe.rb
+++ tictactoe.rb
@@ -1,10 +1,23 @@
 class Board
+  INITIAL_MARKER = "X"
+
   def initialize
+    @squares = {}
+    (1..9).each { |key| @squares[key] = INITIAL_MARKER }
+  end
+
+  def get_square_at(key)
+    @squares[key]
   end
 end
 
 class Square
-  def initialize
+  def initialize(marker)
+    @marker
+  end
+
+  def to_s
+    @marker
   end
 end
 
@@ -18,6 +31,8 @@ class Player
 end
 
 class TTTGame
+  attr_reader :board
+
   def initialize
     @board = Board.new
   end
@@ -27,18 +42,18 @@ class TTTGame
     puts ""
   end
 
-  def display_board 
+  def display_board(board)
     puts ""
     puts "     |     |"
-    puts "     |     |"
-    puts "     |     |"
-    puts "-----+-----+-----"
-    puts "     |     |"
-    puts "     |     |"
+    puts "  #{board.get_square_at(1)}  |  #{board.get_square_at(2)}  |  #{board.get_square_at(3)}  "
     puts "     |     |"
     puts "-----+-----+-----"
     puts "     |     |"
+    puts "  #{board.get_square_at(4)}  |  #{board.get_square_at(5)}  |  #{board.get_square_at(6)}  "
     puts "     |     |"
+    puts "-----+-----+-----"
+    puts "     |     |"
+    puts "  #{board.get_square_at(7)}  |  #{board.get_square_at(8)}  |  #{board.get_square_at(9)}  "
     puts "     |     |"
     puts ""
   end
