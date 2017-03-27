260049b68ae2632e6d193eb3a07d5a4470639256
mm86 <madhuvanmahesh@gmail.com>
2017-03-27 09:55:51 -0700

    fix: Replace @squares with the getter methods

--- tictactoe.rb
+++ tictactoe.rb
@@ -1,5 +1,3 @@
-require 'pry'
-
 module ArrayModifications
   def joinor(arr, delimiter=', ', word='or')
     case arr.size
@@ -14,7 +12,7 @@ module ArrayModifications
 end
 
 class Board
-  attr_accessor :squares
+  attr_reader :squares
   WINNING_LINES = [[1, 2, 3], [4, 5, 6], [7, 8, 9]] + # rows
                   [[1, 4, 7], [2, 5, 8], [3, 6, 9]] + # cols
                   [[1, 5, 9], [3, 5, 7]]              # diagonals
@@ -25,11 +23,11 @@ class Board
   end
 
   def []=(num, marker)
-    @squares[num].marker = marker
+    squares[num].marker = marker
   end
 
   def unmarked_keys
-    @squares.keys.select { |key| @squares[key].unmarked? }
+    squares.keys.select { |key| squares[key].unmarked? }
   end
 
   def full?
@@ -51,21 +49,21 @@ class Board
   end
 
   def reset
-    (1..9).each { |key| @squares[key] = Square.new }
+    (1..9).each { |key| squares[key] = Square.new }
   end
 
   # rubocop:disable Metrics/AbcSize
   def draw
     puts "     |     |"
-    puts "  #{@squares[1]}  |  #{@squares[2]}  |  #{@squares[3]}"
+    puts "  #{squares[1]}  |  #{squares[2]}  |  #{squares[3]}"
     puts "     |     |"
     puts "-----+-----+-----"
     puts "     |     |"
-    puts "  #{@squares[4]}  |  #{@squares[5]}  |  #{@squares[6]}"
+    puts "  #{squares[4]}  |  #{squares[5]}  |  #{squares[6]}"
     puts "     |     |"
     puts "-----+-----+-----"
     puts "     |     |"
-    puts "  #{@squares[7]}  |  #{@squares[8]}  |  #{@squares[9]}"
+    puts "  #{squares[7]}  |  #{squares[8]}  |  #{squares[9]}"
     puts "     |     |"
   end
   # rubocop:enable Metrics/AbcSize
@@ -333,7 +331,7 @@ class TTTGame
   end
 
   def assign_current_marker(answer)
-    self.current_marker = if answer == 'human'
+    self.current_marker = if %w[human Human].include?(answer)
                             @human.marker
                           else
                             @computer.marker
