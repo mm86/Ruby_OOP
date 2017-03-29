8ae10fb872d173dd528cb289a7df38a207c78060
mm86 <madhuvanmahesh@gmail.com>
2017-03-29 12:52:45 -0700

    feat: Add methods to calculate and display scores of both players

--- twenty_one.rb
+++ twenty_one.rb
@@ -1,18 +1,52 @@
 
 
 class Deck
-  # responsible for creating the deck of cards
-  # responsible for dealing unique cards from it's deck every time the method deal_two_cards is called.
-  def initialize 
+  SUITS = ['H', 'D', 'S', 'C']
+  VALUES = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
+  attr_reader :deck 
+
+  def initialize
+    @deck = SUITS.product(VALUES).shuffle
   end 
 
-  def deal_two_cards 
+  def deal_two_cards
+    cards = [] 
+    2.times do
+      cards << deck.pop 
+    end
+    cards 
+  end
+
+  def compute_total_score(cards)
+    values = cards.map { |card| card[1] }
+
+    sum = 0
+    values.each do |value|
+      if value == "A"
+        sum += 11
+      elsif value.to_i == 0 # J, Q, K
+        sum += 10
+      else
+        sum += value.to_i
+      end
+    end
+
+    # correct for Aces
+    values.select { |value| value == "A" }.count.times do
+      sum -= 10 if sum > 21
+    end
+
+    sum 
+
   end 
 end 
 
 
 class Player
-
+  attr_accessor :cards, :score 
+  def initialize
+    @score = 0 
+  end
  
 end 
 
@@ -27,17 +61,19 @@ end
 
 
 class Game
+  attr_reader :human, :dealer, :deck
 
   def initialize
     @human = Human.new 
-    @dealer = Dealer.new 
+    @dealer = Dealer.new
+    @deck = Deck.new 
   end
 
   def play
     display_welcome_message
     deal_cards
     show_initial_cards
-
+    compute_and_display_total_score_cards
   end 
 
   private 
@@ -46,13 +82,31 @@ class Game
     puts "Welcome to 21 game"
   end 
 
-  def deal_cards 
+  def deal_cards
+    human.cards = deck.deal_two_cards
+    dealer.cards = deck.deal_two_cards 
   end 
 
-  def show_initial_cards 
+  def show_initial_cards
+    puts "Human cards are #{human.cards}" 
+    puts "Dealer cards are #{dealer.cards}" 
+  end
+
+  def compute_and_display_total_score_cards
+    compute_total_score_cards 
+    display_total_score_cards
+  end
+
+  def compute_total_score_cards
+    human.score = deck.compute_total_score(human.cards)
+    dealer.score = deck.compute_total_score(dealer.cards)
   end 
 
- 
+  def display_total_score_cards
+    puts "Human score is #{human.score}"
+    puts "Dealer score is #{dealer.score}" 
+  end
+
 end 
 
 Game.new.play
