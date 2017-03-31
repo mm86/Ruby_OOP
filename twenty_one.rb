ced0d384eb3fab716ab8c2cdc4a05a1644d101a5
mm86 <madhuvanmahesh@gmail.com>
2017-03-30 21:48:23 -0700

    fix: Fix rubocop offenses

--- twenty_one.rb
+++ twenty_one.rb
@@ -1,34 +1,38 @@
-
+require 'pry'
 
 class Deck
   SUITS = ['H', 'D', 'S', 'C']
   VALUES = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
-  attr_reader :deck 
+  attr_reader :deck
 
   def initialize
     @deck = SUITS.product(VALUES).shuffle
-  end 
-
-  def deal_two_cards
-    cards = [] 
-    2.times do
-      cards << deck.pop 
-    end
-    cards 
   end
 
-  def compute_total_score(cards)
+  def deal_two_cards
+    cards = []
+    2.times do
+      cards << deck.pop
+    end
+    cards
+  end
+
+  def deal_one_card
+    deck.pop
+  end
+
+  def compute_score(cards)
     values = cards.map { |card| card[1] }
 
     sum = 0
     values.each do |value|
-      if value == "A"
-        sum += 11
-      elsif value.to_i == 0 # J, Q, K
-        sum += 10
-      else
-        sum += value.to_i
-      end
+      sum += if value == "A"
+               11
+             elsif value.to_i == 0 # J, Q, K
+               10
+             else
+               value.to_i
+             end
     end
 
     # correct for Aces
@@ -36,108 +40,195 @@ class Deck
       sum -= 10 if sum > 21
     end
 
-    sum 
-
-  end 
-end 
-
+    sum
+  end
+end
 
 class Player
-  attr_accessor :cards, :score 
+  attr_accessor :cards, :score, :status, :name
   def initialize
-    @score = 0 
+    @score = 0
     @cards = []
   end
- 
-end 
 
-class Human < Player
-
-
-end 
-
-class Dealer < Player 
-
-end 
-
+  def busted?
+    score > 21
+  end
+end
 
 class Game
+  DEALER_MAX_SCORE_LIMIT = 17
   attr_reader :human, :dealer, :deck
+  attr_accessor :current_player
 
   def initialize
-    @human = Human.new 
-    @dealer = Dealer.new
-    @deck = Deck.new 
+    @human = Player.new
+    @dealer = Player.new
+    @deck = Deck.new
+    @current_player = @dealer
+    set_names
+  end
+
+  def set_names
+    @human.name = "Madhu"
+    @dealer.name = "Dealer"
   end
 
   def play
     display_welcome_message
     deal_cards
     show_initial_cards
-    compute_and_display_total_score_cards
-    answer = ask_human_hit_or_stay
-    display_human_response(answer)
-  end 
+    loop do
+      self.current_player = alternate_player(current_player)
+      current_player_turn
+      break if current_player.busted? || both_players_stay?
+    end
+    display_winner
+    display_goodbye_message
+  end
 
-  private 
+  private
 
-  def prompt(message) 
+  def prompt(message)
     puts "=> #{message}"
   end
 
   def display_welcome_message
-    prompt "Welcome to 21 game"
-  end 
+    prompt "Welcome to 21 Game"
+  end
 
   def deal_cards
     human.cards = deck.deal_two_cards
-    dealer.cards = deck.deal_two_cards 
-  end 
+    dealer.cards = deck.deal_two_cards
+  end
 
   def show_initial_cards
-    prompt "Human cards are #{human.cards[0]} and #{human.cards[1]}" 
-    prompt "Dealer cards are #{dealer.cards[0]} and ?" 
+    prompt "Human cards are #{human.cards[0]} and #{human.cards[1]}. " \
+           "Human score is #{deck.compute_score(human.cards)}"
+    prompt "Dealer cards are #{dealer.cards[0]} and ?"
   end
 
-  def compute_and_display_total_score_cards
-    compute_total_score_cards 
-    display_total_score_cards
-  end
-
-  def compute_total_score_cards
-    human.score = deck.compute_total_score(human.cards)
-    dealer.score = deck.compute_total_score(dealer.cards)
-  end 
-
-  def display_total_score_cards
-    prompt "Human score is #{human.score}" 
-  end
-
-  def valid_choice?(answer)
-
-    %w[h s hit stay Hit Stay H S].include?(answer) && answer !~ /\A\s*\z/ && !answer.empty?
-  end
-
-  def display_human_response(answer)
-    if %w[h H Hit hit].include?(answer)
-      prompt "You chose to hit"
+  def alternate_player(player)
+    if player.equal?(dealer)
+      human
     else
-      prompt "You chose to stay"
+      dealer
     end
+  end
+
+  def current_player_turn
+    human_turn if current_player.equal?(human)
+    dealer_turn if current_player.equal?(dealer)
+  end
+
+  def update_current_player_score
+    self.current_player.score = deck.compute_score(current_player.cards)
+  end
+
+  def display_cards_and_scores
+    prompt "#{current_player.name} cards are #{current_player.cards}"
+    prompt "#{current_player.name} score is #{current_player.score}"
+  end
+
+  def display_cards_update_score
+    update_current_player_score
+    display_cards_and_scores
+  end
+
+  def valid_response?(answer)
+    %w[h s hit stay Hit Stay H S].include?(answer) &&
+      answer !~ /\A\s*\z/ &&
+      !answer.empty?
   end
 
   def ask_human_hit_or_stay
-    prompt "Would you like to hit or stay? [Valid responses: h s hit stay H S Hit Stay]" 
     answer = nil
-    loop do 
+    loop do
+      prompt "Would you like to hit or stay?"
       answer = gets.chomp
-      break if valid_choice?(answer)
-      prompt "Please enter a valid response"
+      break if valid_response?(answer)
+      prompt "Please enter a valid choice"
     end
-    
-    answer
+    human.status = answer
   end
 
-end 
+  def human_chose_hit?
+    %w[h hit Hit H].include?(human.status)
+  end
+
+  def human_chose_stay?
+    %w[s stay Stay S].include?(human.status)
+  end
+
+  def human_turn
+    prompt "Human's turn .........."
+    loop do
+      ask_human_hit_or_stay
+      if human_chose_hit?
+        prompt "Human chooses to hit ........."
+        human.cards << deck.deal_one_card
+        display_cards_update_score
+        break if human.busted?
+      else
+        prompt "Human chooses to stay"
+        break
+      end
+    end
+  end
+
+  def dealer_busts_or_stays?
+    return true if dealer.busted?
+    if dealer.score >= DEALER_MAX_SCORE_LIMIT
+      prompt "Dealer chooses to stay"
+      current_player.status = 'stay'
+      true
+    end
+  end
+
+  def dealer_turn
+    prompt "Dealer's turn .........."
+    display_cards_update_score
+    loop do
+      prompt "Dealer hits .........."
+      dealer.cards << deck.deal_one_card
+      display_cards_update_score
+      break if dealer_busts_or_stays?
+    end
+  end
+
+  def both_players_stay?
+    %w[s stay Stay S].include?(human.status) &&
+      %w[s stay Stay S].include?(dealer.status)
+  end
+
+  def display_winner_details1
+    if human.score > dealer.score
+      prompt "Human won. Dealer lost."
+    elsif dealer.score > human.score
+      prompt "Dealer won. Human lost."
+    else
+      prompt "Its a tie."
+    end
+  end
+
+  def display_winner_details2
+    if human.busted?
+      prompt "Human busted. Dealer won"
+    else
+      prompt "Dealer busted. Human won"
+    end
+  end
+
+  def display_winner
+    prompt "-------------WINNER IS-----------------"
+    display_winner_details1 if both_players_stay?
+    display_winner_details2 if current_player.busted?
+    prompt "-------------THE END-------------------"
+  end
+
+  def display_goodbye_message
+    prompt "Thanks for playing Twenty-One game"
+  end
+end
 
 Game.new.play
